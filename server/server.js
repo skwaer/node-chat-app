f5b2bdbaab476b8429f9035be22f9099edb3a54d
Paul Rush <paul@PRQZ2.hitronhub.home>
2018-07-28 16:42:57 -0700

    Added things

--- server/server.js
+++ server/server.js
@@ -1,20 +1,30 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
-const http = require('http');
 const port = process.env.PORT || 8080;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 app.use(bodyParser.json());
-
 app.use(express.static(publicPath))
 
-app.listen(port, (err) => {
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.on('disconnect', () => {
+		console.log('disconnected');
+	})
+})
+
+server.listen(port, (err) => {
 	if (err) return console.log('ERROR: could not start server', err);
 
 	console.log(`Started on port ${port}`);
